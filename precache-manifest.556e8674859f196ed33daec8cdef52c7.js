9b0eae040d2358cbe99eb52b21cdf472758b3b54
Erubure Ejiro david <davidejiro49a@gmail.com>
2020-08-02 00:27:10 +0100

    Updates

---
+++ precache-manifest.556e8674859f196ed33daec8cdef52c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6384d65d24150933e2b15f1d77b9273f",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "3e735afc9cd632b952a5",
+    "url": "/todoapp/static/css/main.e1ed66c7.chunk.css"
+  },
+  {
+    "revision": "5b693eb7271a11bcfdfb",
+    "url": "/todoapp/static/js/2.e23b6828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.e23b6828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e735afc9cd632b952a5",
+    "url": "/todoapp/static/js/main.161120ac.chunk.js"
+  },
+  {
+    "revision": "9dbb7bc5724293219aaf",
+    "url": "/todoapp/static/js/runtime-main.2b1cbbaf.js"
+  }
+]);
